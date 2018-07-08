aa1d6eb13a688665e6bb38571d2e010b0a817c61
Rangi <remy.oukaour+rangi42@gmail.com>
2018-07-08 17:29:42 -0400

    Better MissingNo. data formatting

--- data/baseStats/missingno.asm
+++ data/baseStats/missingno.asm
@@ -6,11 +6,11 @@ db 29 ; base speed
 db 6 ; base special
 db BIRD ; species type 1
 db NORMAL ; species type 2
-db 255 ; catch rate
+db 29 ; catch rate
 db 143 ; base exp yield
 db $88 ; sprite dimensions
 dw MissingNoPicFront
-dw $8F37
+dw vChars1 + $737
 ; attacks known at lvl 0
 db WATER_GUN
 db WATER_GUN
@@ -18,5 +18,11 @@ db SKY_ATTACK
 db 0
 db $1A ; growth rate
 ; learnset
-db $37,$37,$0D,$37,$00,$1C,$0D
+	tmlearn 1,2,3,5,6 ; $37
+	tmlearn 9,10,11,13,14 ; $37
+	tmlearn 17,19,20 ; $0D
+	tmlearn 25,26,27,29,30 ; $37
+	tmlearn 0 ; $00
+	tmlearn 43,44,45 ; $1C
+	tmlearn 49,51,52 ; $0D
 db BANK(MissingNoPicFront)
